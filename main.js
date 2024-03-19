50de221c4a2789f4b7dfeb0555de3879769da374
MarwanMaro <marwanessam555@gmail.com>
2024-03-19 04:21:37 +0200

    creative landing page

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let image = document.querySelector(".img-iphone");
+let container = document.querySelector(".container");
+
+function Phones(iphone){
+    image.src = iphone;
+}
+
+function background(colors){
+    container.style.background = colors
+}
